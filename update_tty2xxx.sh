e7ee135a1faafe367bffc27dd24338c55fc6ed6c
Oliver Jaksch <ojaksch@com-in.de>
2022-07-27 17:20:48 +0200

    Additions

--- update_tty2xxx.sh
+++ update_tty2xxx.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
-. /media/fat/Scripts/update_tty2xxx.ini
+. /media/fat/Scripts/
+
+runupdate() {
+  echo "${fyellow}${fblink}${fbold}RUNNING ${1}${freset}"
+  "/media/fat/Scripts/${1}"
+  if [ "${PAUSE}" = "yes" ]; then
+    echo "${fyellow}${fblink}${fbold}Press ENTER to continue.${freset}"
+    read dummy
+  fi
+  [ "${CLEAR}" = "yes" ] && clear
+}
 
 if [ "${i2c2oled}" = "yes" ] && [ -e /media/fat/Scripts/update_i2c2oled.sh ]; then
-  /media/fat/Scripts/update_i2c2oled.sh
+  runupdate update_i2c2oled.sh
 fi
 if [ "${tty2oled}" = "yes" ] && [ -e /media/fat/Scripts/update_tty2oled.sh ]; then
-  /media/fat/Scripts/update_tty2oled.sh
+  runupdate update_tty2oled.sh
 fi
 if [ "${tty2tft}" = "yes" ] && [ -e /media/fat/Scripts/update_tty2tft.sh ]; then
-  /media/fat/Scripts/update_tty2tft.sh
+  runupdate update_tty2tft.sh
 fi
 if [ "${tty2rpi}" = "yes" ] && [ -e /media/fat/Scripts/update_tty2rpi.sh ]; then
-  /media/fat/Scripts/update_tty2rpi.sh
+  runupdate update_tty2rpi.sh
 fi
 if [ "${tty2rgb}" = "yes" ] && [ -e /media/fat/Scripts/update_tty2rgb.sh ]; then
-  /media/fat/Scripts/update_tty2rgb.sh
+  runupdate update_tty2rgb.sh
 fi
 if [ "${update_all}" = "yes" ] && [ -e /media/fat/Scripts/update_all.sh ]; then
-  /media/fat/Scripts/update_all.sh
+  runupdate update_all.sh
 fi
